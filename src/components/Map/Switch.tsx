ab7d1db1974994f10606a664813d08faeb88a0c7
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-18 00:42:04 +0800

    docs(frontend): add documentation for switch component

--- src/components/Map/Switch.tsx
+++ src/components/Map/Switch.tsx
@@ -3,6 +3,14 @@ import type { Map } from "leaflet";
 
 import { OFFICES } from "utils/constants";
 
+/**
+ * Office switch component.
+ * @description Switches between the offices
+ *
+ * @param map The map component that contains the necessary functions to make changes to the Leaflet Map component
+ * @param newOffice Sets the new office
+ * @param changeOffice Callback for when button is pressed
+ */
 function Switch({
   map,
   newOffice,
