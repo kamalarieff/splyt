dac0ba0919d94c4514bad49d76271a66227b59e3
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-18 14:24:43 +0800

    feat(frontend): remove query retries

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import "./index.css";
 import { Map } from "./components/Map";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
